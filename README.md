d4f5231961725088b277ee30165a627a4814c4be
Luis Chunga <luischungar7@gmail.com>
2018-08-30 12:51:32 -0500

    App finalizada

---
+++ README.md
@@ -0,0 +1,12 @@
+# Escucha colaborativa en Spotify
+## Reto propuesto por Media Impact 
+
+Por el momento se ejecuta localmente
+
+*Instalación*:
+
+- Ejecutar `npm install` 
+- Ejecutar `npm run dev`
+
+
+
---
+++ components/ButtonStyle.js
@@ -0,0 +1,26 @@
+export const ButtonStyle = `.btn {
+  background-color: transparent;
+  border: 1px solid #008f39;
+  border-radius: 50px;
+  color: #008f39;
+  cursor: pointer;
+  line-height: 28px;
+  padding: 0 15px;
+  
+}
+.btn:hover {
+  background: #008f39;
+  color: #fff;
+-webkit-transform:scale(1.25);
+-moz-transform:scale(1.25);
+-ms-transform:scale(1.25);
+-o-transform:scale(1.25);
+transform:scale(1.25);
+}
+`;
+
+export const ButtonDarkStyle = `.btn--dark {
+  background-color: #bbc8d5;
+  border: 1px solid #bbc8d5;
+  color: #333;
+}`;
